85d0f6951a2be6aaed8d3653323eb0e837abce2a
alan <cvinny33@gmail.com>
2022-07-07 18:33:16 -0600

    added new for loop

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var generateBtn = document.querySelector("#generate");
   var upperCaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var lowerCaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var specialCharactersChar = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","[","]","{","}",'/',"|", ":", ";", ","," ' ",' "'];
-
-var generatedPwd ;
+  var password = []
+var generatedPwd = [] ;
 // Write password to the #password input
 function generatePassword() {
   var password = writePassword();
@@ -14,42 +14,55 @@ function generatePassword() {
   passwordText.value = password;
 }
 function writePassword(){
+  var password = []
+  var generatedPwd = [] ;
   var passwordLength = prompt("enter length here");
   passwordLength = parseInt(passwordLength);
   if (passwordLength >128 || passwordLength <8) {
     alert("Password length incorrect.");
-    writePassword(); 
     return;
   }
 
   var upperCase = confirm("can you use upperCase?");
   if (upperCase) {
    let passwordChar = upperCaseChar[Math.floor((Math.random()*  (upperCaseChar.length -1)))];
-   generatedPwd.push(passwordChar);
+   password.push(passwordChar);
+   generatedPwd = generatedPwd.concat(upperCaseChar);
 
-  
+   passwordLength--
   }
   var lowerCase = confirm("can you use lowerCase?");
   if (lowerCase) {
     let passwordChar = lowerCaseChar[Math.floor((Math.random()*  (lowerCaseChar.length -1)))];
-   generatedPwd.push(passwordChar);
-
+   password.push(passwordChar);
+   generatedPwd = generatedPwd.concat(lowerCaseChar);
+    passwordLength--
     
   }
   var numeric = confirm("can you use numeric characters?");
   if (numeric) {
     let passwordChar = numericChar[Math.floor((Math.random()*  (numericChar.length -1)))];
-   generatedPwd.push(passwordChar);
-
-    
+   password.push(passwordChar);
+   generatedPwd = generatedPwd.concat(numericChar);
+   passwordLength--
   }
   var specialCharacters = confirm("can you use special characters?");
   if (specialCharacters) {
     let passwordChar = specialCharactersChar[Math.floor((Math.random()*  (specialCharactersChar.length -1)))];
-   generatedPwd.push(passwordChar);
+   password.push(passwordChar);
+   generatedPwd = generatedPwd.concat(specialCharactersChar);
+   passwordLength--
 
-    
   }
+  console.log(generatedPwd);
+  for(var i = 0; i < passwordLength; i++) {
+    var randomI = Math.floor(Math.random()* generatedPwd.length);
+    var randomChar = generatedPwd[randomI];
+    password.push(randomChar);
+
+  }
+   return password.join("");
+
 
     
 
